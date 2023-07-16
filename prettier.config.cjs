d3e18c2d290b3af3648f9537891955b59c2fc714
Kağan Uğur <kgnugur@gmail.com>
2023-07-17 00:00:00 +0300

    chore: Add ts-reset

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,7 +1,5 @@
 /** @type {import('prettier').Config} */
 module.exports = {
   semi: false,
-  singleQuote: true,
-
   plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
-};
+}
