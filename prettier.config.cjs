c01040571f3d8ab303fa6772dec2bddfef701c92
Kağan Uğur <kgnugur@gmail.com>
2023-07-17 00:00:00 +0300

    chore: Add prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,7 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  semi: false,
+  singleQuote: true,
+
+  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
+};
