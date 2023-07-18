75ed59e3ec3615f337cd7aa073a94c51b29fd1cc
Kağan Uğur <kgnugur@gmail.com>
2023-07-19 00:00:00 +0300

    feat: Add header login, logout, profile info

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,18 @@
 import { ThemeToggle } from "@/components/theme-toggle"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import { cn } from "@/lib/utils"
+import { LogOutIcon } from "lucide-react"
 import type { Metadata } from "next"
+import { getServerSession } from "next-auth"
 import { Inter } from "next/font/google"
 import Link from "next/link"
 import "./globals.css"
@@ -13,11 +25,13 @@ export const metadata: Metadata = {
   description: "A Reddit App built with everything new in Next.js 13",
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const session = await getServerSession()
+
   return (
     <html className="h-full" lang="en" suppressHydrationWarning>
       <body
@@ -32,6 +46,56 @@ export default function RootLayout({
               <Link href="/" className="select-none font-bold">
                 Next-Reddit
               </Link>
+
+              <div className="flex-1" />
+
+              {session ? (
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button
+                      variant="secondary"
+                      className="relative h-8 w-8 rounded-full"
+                    >
+                      <Avatar className="h-8 w-8">
+                        <AvatarImage
+                          src={session.user?.image ?? ""}
+                          alt={session.user?.name ?? ""}
+                        />
+                        <AvatarFallback>KK</AvatarFallback>
+                      </Avatar>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="w-56" align="end" forceMount>
+                    <DropdownMenuLabel className="font-normal">
+                      <div className="flex flex-col space-y-1">
+                        <p className="text-sm font-medium leading-none">
+                          {session.user?.name}
+                        </p>
+                        <p className="text-xs leading-none text-muted-foreground">
+                          {session.user?.email}
+                        </p>
+                      </div>
+                    </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem asChild>
+                      <Link href="/api/auth/signout">
+                        <LogOutIcon
+                          className="mr-2 h-4 w-4"
+                          aria-hidden="true"
+                        />
+                        Log out
+                      </Link>
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              ) : (
+                <Button size="sm" asChild>
+                  <Link href="/api/auth/signin">
+                    Sign In
+                    <span className="sr-only">Sign In</span>
+                  </Link>
+                </Button>
+              )}
             </div>
           </header>
 
