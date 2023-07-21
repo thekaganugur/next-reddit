408404267ba7958688fb272b2b5713d7bdc055a1
Kağan Uğur <kgnugur@gmail.com>
2023-07-22 00:00:00 +0300

    feat: Add react-hook-form and sonner toaster

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,11 +2,17 @@
 
 import { ThemeProvider } from "next-themes"
 import React from "react"
+import { Toaster } from "sonner"
 
 type Props = {
   children: React.ReactNode
 }
 
 export function Providers({ children }: Props) {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>
+  return (
+    <ThemeProvider attribute="class">
+      {children}
+      <Toaster />
+    </ThemeProvider>
+  )
 }
