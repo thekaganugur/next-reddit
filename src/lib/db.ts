dde4b0a14309bf77e3a49f08cf1e31f9f8266c25
Kağan Uğur <kgnugur@gmail.com>
2023-07-23 00:00:00 +0300

    feat: Add scheme for subreddit

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,3 +1,8 @@
 import { PrismaClient } from "@prisma/client"
 
-export const prisma = new PrismaClient()
+// https://www.prisma.io/docs/guides/performance-and-optimization/connection-management#prevent-hot-reloading-from-creating-new-instances-of-prismaclient
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+
+export const prisma = globalForPrisma.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
